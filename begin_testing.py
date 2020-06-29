614c17f531913885e8dee6a1032a6f337bf0a4cd
hw <hopefully.purple@gmail.com>
2020-06-29 10:05:50 -0600

    Fixed some bugs, separated begin_testing into two files

--- begin_testing.py
+++ begin_testing.py
@@ -1,42 +1,42 @@
-# This python script is for a VM node in a NDN network that is acting as a client. 
-# There are two options for testing
-# The first is the control case and the second is the victim case
-# 
-# The control case has a few default settings, which can be changed by the user when prompted before it begins behavior.
-# Control Case:
-	# Settings:
-		# Total Number of Requests per Repetition
-		# Number of Repetitions
-		# I have an idea for two other settings, but I'm starting with this much to not get overwhelmed.
-	#Functionality:
-		# The idea (currently) is to take a file and put each string in an array, including duplicates to make it more realistic and decomplicate my algorithm (although this could change)
-		# Then, it will take each item, add it to the /ndn/ prefix, and store it in a new array? Not very efficient . . replace each item with the prefix + item?
-		# Next, it will enter a loop, and for each item in the array, it will send a request/Interest Packet. Now here is the tricky part.
-			# I need to figure out how best to time the router response. Investigate the PyNDN tools that are already there, or try to extract the timestamps from the IP and DP
-		# Once the result from the interest comes, add the timestamp and other information to the stats_file.txt
-		# Loop to the next item.
-		# Once that loop is done, repeat the that previous loop for the given amount of repetitions, making sure there is a separation in the stats_file.txt
-			# This makes me nervous, because at this point the router should have all of these requests in the CS, which means the response time will be faster. 
-			# I also need to decide if I'm going to involve another router, and mix up which one hosts what data. 
-			# I feel that if I want this control case to work properly, these CS and PIT and FIBs need to be wiped clean. 
-			# I guess I could turn off caching on the routers, although I feel that becomes a breach of emulating a real NDN scenario. 
-		# When that is finished, store stats at the end of stats_file.txt and print completion messages.
-			# stats include: reps, total requs, avg router response time
-			# I'm unsure how I'll get it to do that avg math, worry when I get there
-		# Ask user if they want to run it again. Start all the way from the control or victim option.
-
 import math
 import time
-
+from control import Control
 
 def questions():
     """ Function that asks the user questions about the setup for the test"""
-    print("Control Case or Victim?")
+
+    # String that stores whether the user wants the control case or the victim case
+    control_or_victim = input("Control Case or Victim?(c/v): ")
+    # String that stores whether the user wants to adjust the settings or not.
+    adjust_settings = input("Adjust settings?(y/n): ")
+
+    if adjust_settings == "y" and control_or_victim == "c":
+        ctrl = Control(100, 15)
+        print("Total Number of Requests per Repetition =", ctrl.total_reqs, ": ")
+       #if number != "":
+       #    ctrl.total_reqs = number
+        print("Number of Repetitions =", ctrl.reps, ": ")    
+    return control_or_victim
 
 
 def main():
-    #Start by asking the user set up questions
-    questions()
+    # Start by asking the user set up questions
+    case = questions()
+    
+    # Begin running user specified case
+    if case == "c":
+        ctrl = Control(100, 15) #gack this could pose a problem!
+        print("Call control algorithm")
+        ctrl.test()
+        print("Done!")
+    elif case == "v":
+        print("That poor poor victim")
+
+    again = input("Run again?(y/n):")
+    if again == "y":
+        main()
+    elif again == "n":
+        print("Peace out")
 
 
 main()
---
+++ control.py
@@ -0,0 +1,81 @@
+# This python script is for a VM node in a NDN network that is acting as a client. 
+# There are two options for testing
+# The first is the control case and the second is the victim case
+# 
+# The control case has a few default settings, which can be changed by the user when prompted before it begins behavior.
+# Control Case:
+	# Settings:
+		# Total Number of Requests per Repetition
+		# Number of Repetitions
+		# I have an idea for two other settings, but I'm starting with this much to not get overwhelmed.
+	#Functionality:
+		# The idea (currently) is to take a file and put each string in an array, including duplicates to make it more realistic and decomplicate my algorithm (although this could change)
+		# Then, it will take each item, add it to the /ndn/ prefix, and store it in a new array? Not very efficient . . replace each item with the prefix + item?
+		# Next, it will enter a loop, and for each item in the array, it will send a request/Interest Packet. Now here is the tricky part.
+			# I need to figure out how best to time the router response. Investigate the PyNDN tools that are already there, or try to extract the timestamps from the IP and DP
+		# Once the result from the interest comes, add the timestamp and other information to the stats_file.txt
+		# Loop to the next item.
+		# Once that loop is done, repeat the that previous loop for the given amount of repetitions, making sure there is a separation in the stats_file.txt
+			# This makes me nervous, because at this point the router should have all of these requests in the CS, which means the response time will be faster. 
+			# I also need to decide if I'm going to involve another router, and mix up which one hosts what data. 
+			# I feel that if I want this control case to work properly, these CS and PIT and FIBs need to be wiped clean. 
+			# I guess I could turn off caching on the routers, although I feel that becomes a breach of emulating a real NDN scenario. 
+		# When that is finished, store stats at the end of stats_file.txt and print completion messages.
+			# stats include: reps, total requs, avg router response time
+			# I'm unsure how I'll get it to do that avg math, worry when I get there
+		# Ask user if they want to run it again. Start all the way from the control or victim option.
+
+import math
+import time
+from pyndn import Name
+
+class Control():
+    """This class holds settings and algorithms for the Control Case"""
+    def __init__(self, reqs, repits):
+        self.total_reqs = reqs
+        self.reps = repits
+        self.list = []
+
+    def read_file(self):
+        """Reads file and stores each word in a list. Only stores the specified number of requests"""
+        print("Reading File")
+
+        # Open nlsr.conf and read in specified number of words
+        with open('~/nlsr/nlsr.conf', 'r') as input_file:
+            print("succesfully opened file")
+            num = 0
+            for line in input_file:
+                for word in line.split():
+                    if num < self.total_reqs:
+                        num += 1
+                        self.list.append(word)
+                    else:
+                        break
+        print("Populated list")
+        return 0
+
+    def generate_request_strings(self):
+        """Takes the list of words and adds the prefix to them"""
+        print("Generating requests")
+        return 0
+
+    def send_request(self):
+        """Uses PyNDN to send a single request"""
+        print("Send a request")
+        return 0
+
+    def test(self):
+        """ Send requests, and repeat it accordingly"""
+        # Read the given(?) file
+        self.read_file()
+        
+        # Generate requests with list of strings
+        self.generate_request_strings()
+
+        # Loop for the number of repetitions
+        for i in range(0, self.reps):
+            # Send specified number of requests
+            for request in self.list:
+                self.send_request()
+
+
