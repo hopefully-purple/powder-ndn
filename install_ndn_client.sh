4d78232a002d5c3b995298faaaf4f145da88a5ae
hw <hopefully.purple@gmail.com>
2020-07-01 11:29:43 -0600

    Added x11 apps install

--- install_ndn_client.sh
+++ install_ndn_client.sh
@@ -5,6 +5,8 @@ sudo apt-get update
 sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
 sudo pip3 install pyndn
 
+sudo apt-get install x11-apps -y
+
 # copy the client code to the user's home directory
 cp /local/repository/request_data.py ~/
 
