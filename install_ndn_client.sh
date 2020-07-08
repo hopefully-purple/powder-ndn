7d3de096e47afc033a0f3a42d2fe5faf10e0c42f
hw <hopefully.purple@gmail.com>
2020-07-08 13:58:09 -0600

    Commented out everything related to install in an effort to simplify things.

--- install_ndn_client.sh
+++ install_ndn_client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # this bash script installs the NDN python client library for python3
-sudo apt-get update
-sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
-sudo pip3 install pyndn
+#sudo apt-get update
+#sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
+#sudo pip3 install pyndn
 
-sudo apt-get install x11-apps -y
+#sudo apt-get install x11-apps -y
 #sudo apt-get install strace -y
 #sudo apt-get install wireshark -y
 
@@ -26,7 +26,5 @@ cp /local/repository/counter.py ~/
 cp /local/repository/make_requests.py ~/
 
 # copy the two .txt files into the user's home directory
-#cp /local/repository/dictionary.txt ~/
-#cp /local/repository/alicewonderland.txt ~/
 cp /local/repository/alice.txt ~/
 cp /local/repository/whole_dict.txt ~/
