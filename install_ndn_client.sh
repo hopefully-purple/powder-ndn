124ada49c8dcab8bd0c1bf2ba0e589e523e73c3a
hw <hopefully.purple@gmail.com>
2020-07-14 15:03:47 -0600

    Added back in installs

--- install_ndn_client.sh
+++ install_ndn_client.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # this bash script installs the NDN python client library for python3
-#sudo apt-get update
-#sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
-#sudo pip3 install pyndn
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
 
-#sudo apt-get install x11-apps -y
-#sudo apt-get install strace -y
-#sudo apt-get install wireshark -y
+# Maintained version of pyndn currently does not have packet v0.3 support, so alternate version must be built
+git clone https://github.com/Pesa/PyNDN2 ~/PyNDN2
+cd ~/PyNDN2 && git merge remotes/origin/packet03
+pip3 install ~/PyNDN2
 
 # copy the client code to the user's home directory
 cp /local/repository/request_data.py ~/
