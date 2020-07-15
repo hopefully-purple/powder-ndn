105c6fa1bebfa5d85f5db4b1260b4e94fd912dc6
hw <hopefully.purple@gmail.com>
2020-07-14 18:12:09 -0600

    Added code to output a .dat file for a gnuplot script to read

--- run_status_show.sh
+++ run_status_show.sh
@@ -16,12 +16,12 @@ echo "This is the show status output for a unrelated timer, to be run during exp
 #fi
 
 #echo "Default: every 60 seconds for 10 minutes" >> timed_nfdc_status.txt
-echo "Custom: every 1 seconds for 1 minutes" >> timed_nfdc_status.txt
+echo "Custom: every 10 seconds for 1 minutes" >> timed_nfdc_status.txt
 
 
 #read choice
 
-interval=1
+interval=10
 duration=1
 
 #if [[ $choice == "n" ]]; then
@@ -42,15 +42,22 @@ END=$iterations
 echo "Now beginning . . . ."
 echo "Will end in $duration minutes at:"
 date --date="+$totalseconds seconds" +"%Y-%m-%d %H:%M:%S"
-#echo "Before timing loop" >> timed_nfdc_status.txt
 
-#nfdc status show >> timed_nfdc_status.txt
+echo "" > timed_data.dat
 
 for (( c=$START; c<=$END; c++ ))
 do
 	nfdc status show >> timed_nfdc_status.txt
-#	echo -n "$c " >> timed_nfdc_status.txt
+	nfdc status show > temp_nfdc_status.txt
+	awk -F"=" 'NR!=1{print $2 > "temp_timed_data.dat"}' temp_nfdc_status.txt
+	paste timed_data.dat temp_timed_data.dat | awk '{$7="\t"; print > "timed_data.dat"}'
+#awk '{f1 = $0; getline<"temp_timed_data.dat"; print f1, $2}' < temp2_timed_data.dat > timed_data.dat
 	sleep $interval
 done
 
 echo "Finished"
+
+#awk 'BEGIN{FS=OFS="\t fs"}{print $0 OFS value}' timed_nfdc_status.txt
+#awk -F"=" 'BEGIN{FS=OFS="\t fs"}NR!=3{print $2,$0 OFS value > "timed_data.dat"}' timed_nfdc_status.txt
+#awk -F"=" 'NR!=3{print $2 > "temp_timed_data.dat"}' timed_nfdc_status.txt
+
