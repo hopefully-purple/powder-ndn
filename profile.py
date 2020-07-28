44dcda707d50d44de66359b611badec369e84e2f
hw <hopefully.purple@gmail.com>
2020-07-28 09:04:58 -0600

    Put install scripts back in

--- profile.py
+++ profile.py
@@ -59,12 +59,12 @@ def create_nodes(count=2, prefix=1, instantiateOn='pnode', cores=2, ram=4):
         nodes.append(mkVM('node' + str(prefix) + '-' + str(i), GLOBALS.UBUNTU18_IMG, instantiateOn=instantiateOn, cores=cores, ram=ram))
 
     # run install scripts on each vm to install software
-#    for node in nodes:
-#        if node is not None:
+    for node in nodes:
+        if node is not None:
 #           node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install1.sh"))
 #           node.addService(pg.Execute(shell="sh", command="/local/repository/install1.sh"))
-#            node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install_ndn_client.sh"))
-#            node.addService(pg.Execute(shell="sh", command="/local/repository/install_ndn_client.sh"))
+            node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install_ndn_client.sh"))
+            node.addService(pg.Execute(shell="sh", command="/local/repository/install_ndn_client.sh"))
 
     return nodes
 
@@ -85,11 +85,11 @@ def create_routers(instantiateOn='pnode', cores=4, ram=8):
 
     # run alternating install scripts on each vm to install software
 #    odd_router = True
- #   for router in routers:
-  #      if router is not None:
+    for router in routers:
+        if router is not None:
             #if odd_router:
- #           router.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install1.sh"))
- #           router.addService(pg.Execute(shell="sh", command="/local/repository/install1.sh"))
+            router.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install1.sh"))
+            router.addService(pg.Execute(shell="sh", command="/local/repository/install1.sh"))
  #           else:
  #               router.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/install2.sh"))
  #               router.addService(pg.Execute(shell="sh", command="/local/repository/install2.sh"))
