8561131130ba38a7127ea2121d62fc94d75fa66e
hw <hopefully.purple@gmail.com>
2020-08-05 11:30:45 -0600

    Added automation functions

--- install_scripts/router_setup.sh
+++ install_scripts/router_setup.sh
@@ -19,21 +19,11 @@ nfdset() {
 
 	nfdc face create udp4://$ADDR
 
-	#Ask for faceid
-	#read -p "What is the face id? " faceid
-
-	#Create route
-	#echo "Creating route with face id $faceid and prefix /ndn/dictionary"
-	#nfdc route add /ndn/dictionary $faceid
-	
 	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
 	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
 	
 	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
 	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
-
-	#echo "Creating route with face id $faceid and prefix /ndn/attack"
-	#nfdc route add /ndn/attack $faceid
 
 	#Turn off caching?
 	read -p "Turn off caching?(y/n): " caching
@@ -78,6 +68,350 @@ change() {
 
 }
 
+4gr1()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	ADDR=10.10.6.2
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	#----------------
+	echo "Router 2"
+	eth="eth1"
+	latency="3.83333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#----------------
+
+	#----------------
+	echo "Node 1"
+	eth="eth2"
+	latency="3.83333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#----------------
+
+}
+
+4gr2()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	#-------------------
+	echo "Router 1"
+	ADDR=10.10.6.1
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth1"
+	latency="3.83333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 2"
+	eth="eth1"
+	latency="7.66666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 3"
+	eth="eth2"
+	latency="7.66666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 4"
+	eth="eth3"
+	latency="7.66666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 5"
+	eth="eth4"
+	latency="7.66666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Router 3"
+	ADDR=10.10.7.2
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth6"
+	latency="3.83333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+}
+
+4gr3()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	ADDR=10.10.7.1
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth1"
+	latency="3.83333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+
+}
+
+5gr1()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	ADDR=10.10.6.2
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+
+	#----------------
+	echo "Router 2"
+	eth="eth1"
+	latency="0.16666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#----------------
+
+	#----------------
+	echo "Node 1"
+	eth="eth2"
+	latency="0.16666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#----------------
+		
+}
+
+5gr2()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	#-------------------
+	echo "Router 1"
+	ADDR=10.10.6.1
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth1"
+	latency="0.16666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 2"
+	eth="eth1"
+	latency="0.33333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 3"
+	eth="eth2"
+	latency="0.33333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 4"
+	eth="eth3"
+	latency="0.33333333ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Node 5"
+	eth="eth4"
+	latency="0.33333333"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+
+	#-------------------
+	echo "Router 3"
+	ADDR=10.10.7.2
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth6"
+	latency="0.16666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+	#-------------------
+}	
+
+5gr3()
+{
+	read -p "Restart nfd?(y/n): " restart
+	if [ $restart == "y" ]; then
+		echo "Restarting nfd"
+		nfd-stop
+		nfd-start
+	fi
+
+	ADDR=10.10.7.1
+	nfdc face create udp4://$ADDR
+
+	echo "Creating route with prefix /ndn/dictionary to nexthop $ADDR"
+	nfdc route add prefix /ndn/dictionary nexthop udp4://$ADDR
+	
+	echo "Creating route with prefix /ndn/attack to nexthop $ADDR"
+	nfdc route add prefix /ndn/attack nexthop udp4://$ADDR
+	
+	nfdc cs config serve off
+		
+	eth="eth1"
+	latency="0.16666667ms"
+
+	sudo tc qdisc del dev $eth root
+	sudo tc qdisc add dev $eth root netem delay $latency
+	sudo tc qdisc show dev $eth
+
+}
+
+#Automation?
+reap -p "Automation?(4g1/4g2/4g3/5g1/5g2/5g3/n): " auto
+
+if [ "$auto" == "4g1" ]; then
+	4gr1
+elif [ "$auto" == "4g2" ]; then
+	4gr2
+elif [ "$auto" == "4g3" ]; then
+	4gr3
+elif [ "$auto" == "5g1" ]; then
+	5gr1
+elif [ "$auto" == "5g2" ]; then
+	5gr2
+elif [ "$auto" == "5g3" ]; then
+	5gr3
+fi
+
 #Set up nfd?
 read -p "Set up nfd?(y/n): " setnfd
 
@@ -97,7 +431,7 @@ elif [ "$lat" == "r" ]; then
 	sudo tc qdisc del dev $eth root
 elif [ "$lat" == "d" ]; then
 	ifconfig
-	read -p "What is the eht?(format: eth#) " eth
+	read -p "What is the eth?(format: eth#) " eth
 
 	sudo tc qdisc show dev $eth
 fi
