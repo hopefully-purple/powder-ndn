6c6ed27615e1fb49121724d5fadb4d001efea58b
hw <hopefully.purple@gmail.com>
2020-08-09 10:31:18 -0600

    Commented

--- begin_testing.py
+++ begin_testing.py
@@ -1,3 +1,5 @@
+#This script starts the testing of NDN from the client/node side. 
+
 import math
 import os
 import time
@@ -11,17 +13,26 @@ def questions(ctrl):
     # String that stores whether the user wants the control case or the victim case
     control_or_victim = input("Control Case or Victim or Attacker?(c/v/a/p): ")
 
+    # V means the preset victim case
+    # A means the preset attack case
+    # P means plot the most recent test
     if control_or_victim == "v":
         return control_or_victim
     elif control_or_victim == "a":
         return control_or_victim
     elif control_or_victim == "p":
+        #In order to plot the most recent test, it needs the req, rep and datetime information
         requs = input("requests = ")
         repets = input("repetitions = ")
-        con = Convert(requs, repets, "2020-07-28 14:58:13.755885", "2020-07-28 14:58:35.611365")
+        start = input("Start datetime: ")
+        end = input("End datetime: ")
+        con = Convert(requs, repets, start, end)
         con.setup()
         return control_or_victim
 
+    #If the input was 'c', continue here
+
+    #Ask for settings information
     num_reqs = input(f"Total Number of Requests per Repetition = {ctrl.total_reqs} : ")
     if num_reqs != "":
         ctrl.total_reqs = int(num_reqs) 
@@ -30,10 +41,11 @@ def questions(ctrl):
         ctrl.reps = int(num_reps)    
     
     wipe = input("Wipe stats_file?(y/n):")
-        
+   
+    #Set up the stats file with information on the settings 
     if wipe == "y":
         with open('data_collection/stats_file.txt', 'w') as stats:
-            stats.write("A clean slate!\n") #Eventually print out the settings
+            stats.write("A clean slate!\n")
             stats.write(f"Requests per Rep: {ctrl.total_reqs}\t Total Reps: {ctrl.reps}\n")
             stats.write("CONTROL CASE\n")
     elif wipe == "n":
@@ -50,6 +62,7 @@ def questions(ctrl):
     
 
 def victim(ctrl):
+    """ This is the preset victim case for testing NDN. Sets up the ctrl object and the stats file """
     ready = input("Hit enter when ready . . .")
     
     if ready != "":
@@ -66,6 +79,7 @@ def victim(ctrl):
         stats.write("VICTIM CASE\n")
 
 def attacker(ctrl):
+    """ This is the preset attacker case for testing NDN. Sets up the stats file and calls the ctrl.attack function"""
     print("Attack protocol will populate the same files as a regular use case.")
 
     ready = input("Hit enter when ready . . .")
@@ -85,11 +99,12 @@ def attacker(ctrl):
     print(datetime.now())
 
 def main():
-
+    """ Main method, calls other functions for set up """
     print("___________________")
     print("********Ctrl C if you haven't updated the make_request ip address!******")
     print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")    
 
+    # Default settings
     ctrl = Control(100, 15)
 
     # Start by asking the user set up questions
