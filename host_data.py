17a287c90ec83cb241d419df8d0a8d2050c8a339
hw <hopefully.purple@gmail.com>
2020-08-09 10:45:03 -0600

    Commented

--- host_data.py
+++ host_data.py
@@ -1,4 +1,4 @@
-#This will be the host data script for the routers. It would be best in the end to make it in python. 
+#This script is for testing NDN from the router side.
 
 import os
 import subprocess
@@ -7,7 +7,7 @@ import time
 from datetime import datetime
 
 def read_file(requests):
-	
+	""" Function that reads the whole_dict.txt file and poplulates and returns a list according to the number of requests to be satisfied """	
 	data_list = []
 
 	with open('code/whole_dict.txt', 'r') as alice:
@@ -23,11 +23,13 @@ def read_file(requests):
 	return data_list
 
 def host(data_list):
+	""" Function that hosts all the satisfiable requests. Command is put in the background and only looped through once """
 
 	os.system("echo "" > data_collection/echo_times.txt")
 
 	for data in data_list:
 		prefix = "/ndn/dictionary/" + data
+		#Run the command from ndntools that hosts data under a name
 		os.system(f"echo {data} | ndnpoke {prefix} &")
 		time = datetime.now()
 		with open('data_collection/echo_times.txt', 'a') as times:
@@ -35,6 +37,7 @@ def host(data_list):
 
 
 def main():
+	""" Main function that sets up the settings for hosting data """
 
 	#begin ndndump
 	os.system("sudo ndndump -i any -v > data_collection/ndndump.txt &")
